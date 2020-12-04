4e1649df35a95834f66d3a59a670225842ff0753
damage <jphandll@gmail.com>
2020-12-04 13:32:12 +0300

    damage
    lab1-7

--- lab1-6/script/lab6.js
+++ lab1-6/script/lab6.js
@@ -7,33 +7,48 @@ btn.onclick = function func() {
                   X
                   </button>
                   <div class="contentPhone">My phone:123456789<br>My address:China,Shanghai,Renminlu#12</div>`;
-    cont.setAttribute('id','cont-div');
-    cont.setAttribute('class','rainbow');
+    cont.setAttribute('id', 'cont-div');
+    cont.setAttribute('class', 'rainbow');
     cont.style.cssText = `
                 display:flex;background:#00BCD4;align-items:flex-start;align-content:flex-start;
                 justify-content:center;flex-direction:column;
                 left:50%;top:50%;margin-left:-150px;margin-top:-140px;
-                position:fixed;
+                position:absolute;
                 border:1px solid #666;
                 box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.14);
                 `;
-    
-    w = document.getElementsByTagName('body')[0].appendChild(cont);
+
+    w = document.getElementById('cont_telephone').appendChild(cont);
     btn_del = document.getElementById('btn-del');
-    btn_del.style.cssText="position:relative;left:250px;top:-5px;";
+    btn_del.style.cssText = "position:relative;left:250px;top:-5px;";
 
     btn_del.onclick = function del() {
         cont.parentNode.removeChild(cont);
     }
 }
 
-function createWindow(path){
-    window.open(path, "displayWindow", "toolbar=no,width=1440,height=900,directories=no,status=no,scrollbars=yes,resize=no,menubar=no")
+function createWindow(path) {
+    bigImg = document.createElement("div");
+    bigImg.innerHTML = `
+                  <button id="btn-del">
+                  X
+                  </button>`;
+    bigImg.innerHTML= bigImg.innerHTML+"<img "+" src="+path+"></div>"
+    bigImg.setAttribute('id', 'bigImg');
+    //bigImg.setAttribute('class', 'rainbow');
+    bigImg.style.cssText = "overflow:scroll;left:5%;top:5%;width:1400px;height:700px;position:fixed;";
+
+    w = document.getElementsByTagName('body')[0].appendChild(bigImg);
+    btn_del = document.getElementById('btn-del');
+    btn_del.style.cssText = "position:fixed;left:3.4%;top:5%;";
+    btn_del.onclick = function del() {
+        bigImg.parentNode.removeChild(bigImg);
+    }
 }
 
 
 var imgs = document.getElementsByClassName("canada")
 for (let i = 0; i < imgs.length; i++) {
     path = "./img/canada/" + (i + 1).toString() + ".jpg"
-    imgs[i].setAttribute("onclick","createWindow('"+path+"')");
+    imgs[i].setAttribute("onclick", "createWindow('" + path + "')");
 }
--- lab1-6/script/lab7.js
+++ lab1-6/script/lab7.js
@@ -4,16 +4,44 @@ window.onload = function(){
     var TalkSub = document.getElementById("talksub");
     
 
+    var who = ["I","One of my friend","My sister","You and me"];
+    var can = ["can","cannot","could","will"];
+    var doing = ["eat","pick up","throw away","play with"];
+    var what = ["a banana","a ball","a car","superman","sadness","anything"];
+
+    function randomPick(arr)
+    {
+        return arr[Math.floor(Math.random()*arr.length)];
+    }
+    function randomSay(){
+        return randomPick(who)+" "+randomPick(can)+" "+randomPick(doing)+" "+randomPick(what)+"!";
+    }
+
+
     TalkSub.onclick = function(){
         var str = "";
         if(TalkWords.value == ""){
-            alert("消息不能为空");
             return;
         }
-        str = '<div class="btalk"><span>B说 :' + TalkWords.value +'</span></div>' ;  
+        str = '<div class="talk mytalk"><a href = "javascript:;" class = "del">x</a><span>' + TalkWords.value +'</span>'+'</div>' ;  
         Words.innerHTML = Words.innerHTML + str;
-        str = '<div class="atalk"><span>bot :' + "hello" +'</span></div>' ; 
+
+        var patt_expression = /([0-9]+\s*[\+\-\*\/])+\s*/;
+        if(patt_expression.test(TalkWords.value))
+        {
+            str = '<div class="talk bottalk"><span>' + eval(TalkWords.value) +'</span><a href = "javascript:;" class = "del">x</a></div>' ;
+        }
+        else{
+            str = '<div class="talk bottalk"><span>' + randomSay() +'</span><a href = "javascript:;" class = "del">x</a></div>' ; 
+        }
         Words.innerHTML = Words.innerHTML + str;
+        var del = document.getElementsByClassName("del");
+        for (var i = 0; i < del.length; i++) {
+            del[i].onclick = function(){
+                Words.removeChild(this.parentNode);
+            }
+        }
+        TalkWords.value ="";
     }
     
 }
